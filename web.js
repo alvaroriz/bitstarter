50f50fe23f5d21d376f8101d888df8cc7e2b9dc3
Alvaro <alvaroriz@gmail.com>
2013-07-11 21:23:45 -0300

    Updated web.js

--- web.js
+++ web.js
@@ -1,4 +1,12 @@
-var content;
-var f = fs.readFile('./index.html', /* ... */);
+var express = require('express');
 
-f();
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World 2!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
