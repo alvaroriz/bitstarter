b0227e2d218fa5b25fe06a59407f55955e09b19f
Alvaro <alvaroriz@gmail.com>
2013-07-11 21:27:44 -0300

    Updated web.js

--- web.js
+++ web.js
@@ -6,6 +6,14 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
+var fs = require('fs');
+fs.readFile('./index.html', 'utf8', function(err, data) {
+  // the data is passed to the callback in the second argument
+  console.log(data);
+});
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
